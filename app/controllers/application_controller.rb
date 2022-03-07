e252a62b6a5cb0cbcd3ab9f805d456e84d2c2c4d
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-06 22:49:44 -0600

    finall setup of application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,35 @@
 class ApplicationController < ActionController::API
+
+    respont_to :json
+    before_action :process_token
+
+    private 
+    
+    def process_token
+        if request.headers['Authorization'].present?
+          begin
+            jwt_payload = JWT.decode(request.headers['Authorization'].split(' ')[1], Rails.application.secrets.secret_key_base).first
+            @current_user_id = jwt_payload['id']
+          rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+            head :unauthorized
+          end
+        end
+    end
+
+    def authenticate_user!(options={})
+        head :unauthorized unless signed_in?
+    end
+
+    def signed_in?
+        @current_user_id.present?
+    end
+
+    def current_user
+        @current_user ||= super || User.find(@current_user_id)
+    end
+
+
+
+
+
 end
