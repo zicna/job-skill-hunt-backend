8506e74334d1575bbae9c098fe36d0f0a43d1eab
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-06 23:20:06 -0600

    add and setup registrations controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,13 @@
+class RegistrationsController < Devise::RegistrationsController
+
+    def create
+        user = User.new(sign_up_params)
+
+        if user.save
+            token = user.generate_jwt
+            render jason: token.to_json
+        else
+            render json: { errors: {'email or password' => ['is invalid'] } }, status: :unprocessable_entity
+        end
+    end
+end
