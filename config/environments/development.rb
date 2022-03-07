9a3a379c736015302380308538284c434efeacfe
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-03-06 18:17:24 -0600

    set up the default URL options for the Devise mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,6 +32,10 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
+  # !added 
+  # set up the default URL options for the Devise mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.action_mailer.perform_caching = false
 
